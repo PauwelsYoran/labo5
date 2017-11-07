c263b7d36028b290b64dba333375ab22c47f1590
yoran <yoran.pauwels@telenet.be>
2017-11-07 11:49:15 +0100

    qqq

---
+++ Garbage/Garbage/GetRegistrations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Garbage.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Garbage
+{
+    public static class GetRegistrations
+    {
+
+        private static string ConnectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+        [FunctionName("GetRegistrations")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "garbage/getRegistrations")]HttpRequestMessage req, TraceWriter log)
+        {
+            //try
+            //{
+                List<GarbageRegistration> registrations = new List<GarbageRegistration>();
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        string sql = "SELECT * From GarbageRegistration";
+                        command.CommandText = sql;
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            GarbageRegistration regis = new GarbageRegistration();
+                            regis.CityId = Guid.Parse(reader["CityId"].ToString());
+                            regis.Description = reader["Description"].ToString();
+                            regis.Street = reader["Street"].ToString();
+                            regis.TimeStamp = DateTime.Parse(reader["Timestamp"].ToString());
+                        regis.Name = reader["Name"].ToString();
+                            Debug.WriteLine(regis.Lat);
+
+                            //regis.Lat = float.Parse(reader["Lat"].ToString());
+                            //regis.Long = float.Parse(reader["Long"].ToString());
+                            regis.Weight = int.Parse(reader["Weight"].ToString());
+                            regis.Email = reader["Email"].ToString();
+                            regis.GarbageRegistrationid = Guid.Parse(reader["GarbageRegistrationid"].ToString());
+                            regis.GarbagetypeId = Guid.Parse(reader["GarbagetypeId"].ToString());
+                        registrations.Add(regis);
+                        }
+                    }
+                }
+                return req.CreateResponse(HttpStatusCode.OK, registrations);
+
+            //}
+//catch (Exception ex)
+           // {
+           //     return req.CreateResponse(HttpStatusCode.InternalServerError);
+            //}
+        }
+    }
+}
+
--- Garbage/Garbage/Models/GarbageRegistration.cs
+++ Garbage/Garbage/Models/GarbageRegistration.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Garbage.Models
 {
     class GarbageRegistration
     {
+        [JsonProperty("garabageregistrationid")]
         public Guid GarbageRegistrationid { get; set; }
+        [JsonProperty("description")]
         public string Description { get; set; }
+        [JsonProperty("street")]
         public string Street { get; set; }
+        [JsonProperty("garbagetypeid")]
         public Guid GarbagetypeId{ get; set; }
+        [JsonProperty("cityid")]
         public Guid CityId { get; set; }
+        [JsonProperty("weight")]
         public int Weight { get; set; }
+        [JsonProperty("lat")]
         public float Lat { get; set; }
+        [JsonProperty("long")]
         public float Long { get; set; }
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("email")]
         public string Email { get; set; }
+        [JsonProperty("timestamp")]
         public DateTime TimeStamp { get; set; }
     }
 
